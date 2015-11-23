89bd81832c8aca48671537d8addaa1460c1ac0d2
Prawee Wongsa <prawee@hotmail.com>
2015-11-23 15:37:21 +0700

    add accounts.ui to load on client

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -2,7 +2,7 @@
 Tasks=new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
-    angular.module('simple-todos',['angular-meteor']);
+    angular.module('simple-todos',['angular-meteor','accounts.ui']);
     angular.module('simple-todos').controller('TodosListCtrl',[
         '$scope','$meteor',function($scope,$meteor){
             /*$scope.tasks=[
